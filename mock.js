b5e3b6374a07ec7901b324746d5bf30933dbe2ca
NateTheGreat <73970984+nateethegreat@users.noreply.github.com>
2023-03-09 17:11:18 -0500

    mock.js
    This is the main command.

---
+++ mock.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('mock')
+    .setDescription('Ask me to repeat something.'),
+    async execute(interaction) {
+  
+        const modal = new ModalBuilder()
+        .setCustomId('mock_modal') // Changing not recommended unless you know what you're doing.
+        .setTitle('Mock');
+        
+        const text = new TextInputBuilder()
+        .setCustomId('text') // Changing not recommended unless you know what you're doing.
+        .setLabel("What do you want me to say?")
+        .setMaxLength(100) // Can be changed to anything
+        .setStyle(TextInputStyle.Paragraph); // Recommended to keep on Paragraph
+        
+        const secondActionRow = new ActionRowBuilder().addComponents(text);
+        
+        modal.addComponents(secondActionRow);
+        interaction.showModal(modal);
+    }
+}
